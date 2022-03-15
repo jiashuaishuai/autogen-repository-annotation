9fe0921416f0fb03e6719c3449db6ce0fc24fd8f
jss <shuaishuai.jia@raykite.com>
2022-03-15 16:14:51 +0800

    1.0.1

--- autogen-annotation/src/main/java/com/github/annotation/DeprecatedApi.kt
+++ autogen-annotation/src/main/java/com/github/annotation/DeprecatedApi.kt
@@ -4,6 +4,7 @@ package com.github.annotation
 /**
  * apt获取的Deprecated过期注解是java.lang下的注解，不是kotlin Annotations中的过期注解，所以用DeprecatedApi来替代
  */
+@Deprecated("弃用，在生成Java代码时，直接使用java.lang下的Deprecated")
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.BINARY)
 annotation class DeprecatedApi(
