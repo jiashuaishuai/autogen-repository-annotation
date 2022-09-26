b6c65032a4a64c40133997a4f076cd59321e73b9
jss <shuaishuai.jia@raykite.com>
2022-09-26 14:39:49 +0800

    ThreadSchedulerType 添加只处理结果类型

--- autogen-annotation/src/main/java/com/github/annotation/ThreadSchedulerType.kt
+++ autogen-annotation/src/main/java/com/github/annotation/ThreadSchedulerType.kt
@@ -6,10 +6,11 @@ package com.github.annotation
  * @property HANDLE_RESULT_TO_IO 处理结果到IO线程
  * @property SWITCH_IO 直接切换至IO线程
  * @property DO_NOT_HANDLE 不做任何处理，使用返回值默认操作
+ * @property ONLY_HANDLE_RESULT 只处理结果不做线程操作
  */
 enum class ThreadSchedulerType {
 
-    HANDLE_RESULT_TO_MAIN, SWITCH_MAIN, HANDLE_RESULT_TO_IO, SWITCH_IO,DO_NOT_HANDLE
+    HANDLE_RESULT_TO_MAIN, SWITCH_MAIN, HANDLE_RESULT_TO_IO, SWITCH_IO,DO_NOT_HANDLE,ONLY_HANDLE_RESULT
 
 
 }
