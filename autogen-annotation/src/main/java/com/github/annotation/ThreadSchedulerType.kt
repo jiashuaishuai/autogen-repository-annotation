116ff43d08d42c60c5b5d55ea34b79fe9947e38a
jss <shuaishuai.jia@raykite.com>
2022-09-26 15:53:44 +0800

    添加默认操作

--- autogen-annotation/src/main/java/com/github/annotation/ThreadSchedulerType.kt
+++ autogen-annotation/src/main/java/com/github/annotation/ThreadSchedulerType.kt
@@ -5,12 +5,13 @@ package com.github.annotation
  * @property SWITCH_MAIN 直接切换到主线程
  * @property HANDLE_RESULT_TO_IO 处理结果到IO线程
  * @property SWITCH_IO 直接切换至IO线程
- * @property DO_NOT_HANDLE 不做任何处理，使用返回值默认操作
+ * @property DO_NOT_HANDLE 不做任何处理
  * @property ONLY_HANDLE_RESULT 只处理结果不做线程操作
+ * @property DEFAULT 使用默认处理操作
  */
 enum class ThreadSchedulerType {
 
-    HANDLE_RESULT_TO_MAIN, SWITCH_MAIN, HANDLE_RESULT_TO_IO, SWITCH_IO,DO_NOT_HANDLE,ONLY_HANDLE_RESULT
+    HANDLE_RESULT_TO_MAIN, SWITCH_MAIN, HANDLE_RESULT_TO_IO, SWITCH_IO, DO_NOT_HANDLE, ONLY_HANDLE_RESULT, DEFAULT
 
 
 }
